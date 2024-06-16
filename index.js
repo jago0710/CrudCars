ce7cc7c0c72b65533c9b43a1c6e9840ae1b2970c
jago0710 <55157653@iespoblenou.org>
2024-06-16 23:18:25 +0200

    add line for  res in get

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.listen(port)
+
+
+app.get("/", (req, res) => {
+    res.send("Is good!")
+})
+
+
 console.log(`Listen on Port ${port}`);
